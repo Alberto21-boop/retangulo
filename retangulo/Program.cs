523a69e01263aa397600fa7dc54125884c48d77d
Alberto <albertobarbosa0003@gmail.com>
2022-08-27 09:57:49 -0300

    Adicionar arquivos de projeto.

---
+++ retangulo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace retangulo;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Fazer um programa para ler as medidas da base e altura de um fretangulo. Em seguida mostrar o valor da area,
+        //perimetro e diagonal deste retangulo, com quatro casas descimais, conforme exemplos.
+        
+        double area, perimetro, diagonal, Base, altura, Diagonal;
+
+        Console.Write("Entre com a base do retangulo por favor: ");
+        Base = double.Parse(Console.ReadLine());
+
+        Console.Write("Entre com a altura do retangulo por favor: ");
+        altura = double.Parse(Console.ReadLine());
+
+        area = Base * altura;
+
+        perimetro = (Base * 2) + (altura * 2);
+
+        diagonal = Math.Sqrt(Base * Base + altura * altura);
+
+        Console.WriteLine("A area do retangunlo é = " + area.ToString("C"));
+        
+        Console.WriteLine("A area do perimetro é = " + perimetro.ToString("C"));
+        
+        Console.WriteLine("A area do diagonal é = " + diagonal.ToString("C"));
+    }
+}
